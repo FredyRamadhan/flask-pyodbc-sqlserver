d062d4113991aba5e23bc1e819123871d3bc7912
FredyRamadhan <fredytri.ramadhan@gmail.com>
2024-10-03 13:36:47 +0700

    Update and rename ENTITY_PASIEN.sql to entity-pasien.sql

--- entity-pasien.sql
+++ entity-pasien.sql
@@ -1,12 +1,11 @@
 DROP DATABASE IF EXISTS pasien;
 CREATE DATABASE IF NOT EXISTS pasien;
-
 USE databaseKelompok4;
 
 CREATE TABLE pasien (
-	[id_pasien]			VARCHAR(100)	PRIMARY KEY,
-	[nama_pasien]		VARCHAR(100)	NOT NULL,
-	[keluhan]			VARCHAR(50)		NOT NULL,
+	[id_pasien]	VARCHAR(100)	PRIMARY KEY,
+	[nama_pasien]	VARCHAR(100)	NOT NULL,
+	[keluhan]	VARCHAR(50)	NOT NULL,
 	/*
 	[tanggal_lahir]		DATE			NOT NULL,
 	[id_dokter]			VARCHAR(10)		FOREIGN KEY(),
@@ -16,7 +15,6 @@ CREATE TABLE pasien (
 );
 
 INSERT INTO pasien (id_pasien, nama_pasien, keluhan)
-VALUES
-	('P0122', 'Bejo Sutejo', 'demam'),
+VALUES	('P0122', 'Bejo Sutejo', 'demam'),
 	('P0123', 'Bejo Suteji', 'pilek'),
 	('P0124', 'Bejo Suteja', 'batuk');

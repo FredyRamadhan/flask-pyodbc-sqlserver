198b8985633310e625970fbcefc1593e2103b8c7
fredyramadhan <fredytri.ramadhan@gmail.com>
2024-12-20 18:56:52 +0700

    updated layout, added dashboard

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,9 @@ from app.query_functions import create, select, update, delete, other_queries
 
 routes = Blueprint('routes', __name__)
 
-
-
 @routes.route('/')
 def index():
     return render_template('home.html')
-
-
 
 @routes.route('/dokter')
 def dokter():
@@ -18,9 +14,7 @@ def dokter():
         Select * from dokter
         order by id_dokter
         ''')
-    return render_template('dokter.html', table=table)
-
-
+    return render_template('table_dokter.html', table=table)
 
 @routes.route('/dokter/by_spesialisasi')
 def dokter_by_sp():
@@ -28,9 +22,7 @@ def dokter_by_sp():
         select * from dokter
         where spesialisasi = 'umum'
         order by id_dokter''')
-    return render_template('dokter.html', table=table)
-
-
+    return render_template('table_dokter.html', table=table)
 
 @routes.route('/dokter/create', methods=['GET', 'POST'])
 def create_dokter():
@@ -45,9 +37,7 @@ def create_dokter():
         columns = ", ".join(['id_dokter', 'nama_dokter', 'spesialisasi', 'telp_dokter', 'jadwal_praktik'])
 
         create('dokter', columns, data)
-    return render_template('createDokter.html')
-
-
+    return render_template('insert_dokter.html')
 
 @routes.route('/dokter/update/<id_dokter>', methods=['GET', 'POST'])
 def update_dokter(id_dokter):
@@ -55,61 +45,46 @@ def update_dokter(id_dokter):
         new_name = request.form['nama_dokter']
         update(id_dokter, new_name)
         return redirect(url_for('routes.dokter'))
-    table = select("select nama_dokter from dokter where id_dokter = 'd001'")
-    return render_template('editDokter.html', table=table[0])
-
-
+    table = select(f"select nama_dokter from dokter where id_dokter = '{id_dokter}'")
+    return render_template('update_dokter.html', table=table[0])
 
 @routes.route('/dokter/delete/<id_dokter>', methods=['POST'])
 def delete_continent(id_dokter):
-    # Get a connection to the database
-    conn = create_connection()
-    
-    # Check if the connection was successful
-    if conn:
-        cursor = conn.cursor()
-        try:
-            # Delete the dokter from the database
-            cursor.execute('DELETE FROM dokter WHERE id_dokter = ?', (id_dokter))
-            conn.commit()  # Commit the transaction
-            
-            # Redirect to the dokter list with a success message
-            flash('Table A deleted successfully!', 'success')
-        except Exception as e:
-            flash(f'Error: {str(e)}', 'danger')
-        finally:
-            cursor.close()
-            conn.close()  # Ensure the connection is closed
-    else:
-        flash('Error: Unable to connect to the database.', 'danger')
-    
-    return redirect(url_for('routes.dokter'))
-
-
-
-
-@routes.route('/dokter/spesialisasi', methods=[ 'POST' ])
-def filter():
-    column = request.form('kolom_filter')
     conn = create_connection()
     if conn:
         cursor = conn.cursor()
         try:
-            cursor.execute('SELECT * FROM dokter WHERE spesialisasi = ?', (column))
+            cursor.execute('DELETE FROM dokter WHERE id_dokter = ?', (id_dokter,))
             conn.commit()
-            flash('filter berdasarkan spesialisasi')
+            flash('Dokter deleted successfully!', 'success')
         except Exception as e:
             flash(f'Error: {str(e)}', 'danger')
         finally:
             cursor.close()
             conn.close()
     else:
-        flash('Error: unable to connect to the database', 'danger')
+        flash('Error: Unable to connect to the database.', 'danger')
+    
+    return redirect(url_for('routes.dokter'))
+
+@routes.route('/dokter/spesialisasi', methods=['POST'])
+def filter():
+    column = request.form['kolom_filter']
+    conn = create_connection()
+    if conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('SELECT * FROM dokter WHERE spesialisasi = ?', (column,))
+            conn.commit()
+            flash('Filter applied based on spesialisasi')
+        except Exception as e:
+            flash(f'Error: {str(e)}', 'danger')
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        flash('Error: Unable to connect to the database', 'danger')
+    
     return redirect(url_for('routes.dokter'))
 
 
-@routes.route('/testestes')
-def testestes():
-    other_queries('''
-                                                     
-        ''')

51ea702b9045c6bc616d2c23d2de6a7085ab2f01
divamuth <divamuthiah325@gmail.com>
2024-12-21 09:28:58 +0700

    sv

--- connect.py
+++ connect.py
@@ -4,7 +4,7 @@ import os
 # function to create a connection to the database
 def create_connection():
 
-    server = 'DESKTOP-GS6KAEE'  # Ubah sesuai dengan nama server kalian     #
+    server = 'LAPTOP-8DP8TTV8'  # Ubah sesuai dengan nama server kalian     #
     database = 'KLINIK'        # Ubah sesuai dengan nama database kalian   #
 
     connection_string = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database}'
